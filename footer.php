be7708d1e8cb5f942ee17c92ffac87dffadad460
soyuka <soyuka@gmail.com>
2014-05-29 15:12:46 +0200

    Footer refractor

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <!-- Footer -->
 	<footer>
         <p>&copy; <?php echo date("Y") . ' ' . htmlspecialchars($CONF['url']); // You can change this, but it would be lovely if you'd give us your support by keeping our link here :)?><br />
-		<?php echo $lang['Powered by'] ?> <a href="http://sourceforge.net/projects/phpaste/">PASTE</a> <a href="http://php.net/">(PHP)</a>. <?php echo $lang['Frontend'] ?> <a href="http://jquery.com/"><?php echo $lang['jQuery'] ?></a> &amp; <a href="https://github.com/twbs/bootstrap"><?php echo $lang['Twitter Bootstrap'] ?></a>.</p>
+		<?php echo $lang['Powered by'] ?> <a href="http://sourceforge.net/projects/phpaste/">PASTE</a> <a href="http://php.net/">(PHP)</a>. <?php echo $lang['Frontend'] ?> <a href="http://angularjs.org/">angularjs</a> &amp; <a href="https://getboostrap.com"><?php echo $lang['Twitter Bootstrap'] ?></a>.</p>
 	</footer>
 </body>
 </html>
