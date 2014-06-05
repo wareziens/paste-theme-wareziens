38a2b4bbf15af14423d417431f3b607026e17efe
soyuka <soyuka@gmail.com>
2014-06-06 01:51:02 +0200

    Trends

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-angular.module('paste', ['ui.bootstrap', 'ngClipboard'])	
+angular.module('paste', ['ui.bootstrap', 'ngClipboard', 'ngTable'])	
 .config(function(ngClipProvider) {
   ngClipProvider.setPath("templates/wareziens/bower_components/zeroclipboard/ZeroClipboard.swf");
 })
@@ -30,4 +30,49 @@ angular.module('paste', ['ui.bootstrap', 'ngClipboard'])
 			}			
 		}
 	};
-});
+})
+
+.controller('trendCtrl', function($scope, ngTableParams, $http, $filter){
+		
+		$scope.tableParams = new ngTableParams({
+      page: 1,
+      count: 10,
+      sorting: {
+      	hits: 'desc'
+      }
+    }, {
+      total: 0,
+      counts: [], 
+      getData: function($defer, params) {
+
+				$http({method: 'GET', url: window.location.pathname + '?json=true'}).
+			    success(function(data, status, headers, config) {
+
+			    	for(var i in data) {
+			    		data[i].hits = parseFloat(data[i].hits)
+			    		data[i].age = parseFloat(data[i].age)
+			    		data[i].pid = parseFloat(data[i].pid)
+			    		console.log(data[i])
+			    	}
+
+			    	console.log(params.orderBy())
+
+						params.total(data.length)
+
+						data = params.sorting() ?
+                    $filter('orderBy')(data, params.orderBy()) :
+                    data
+
+		        $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()))
+			    }).
+			    error(function(data, status, headers, config) {
+			      console.error('%s: Error occured while getting Trends', status)
+			    })
+
+        // var orderedData = params.sorting() ?
+        //         $filter('orderBy')(data, params.orderBy()) :
+        //         data;
+
+			}
+    })
+})
--- trends.php
+++ trends.php
@@ -1,41 +1,56 @@
 
 <?php
 if($page['trends']) {
-?>
-<table id="trends" class="display" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-                <th>Date</th>
-                <th>Nom</th>
-                <th>Expiration</th>
-                <th>Synthaxe</th>
-                <th>Vues</th>
-                <th>Auteur</th>
-            </tr>
-        </thead>
- 
+  ?>
+
+<div class="row">
+  <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 ">
+    <div class="panel panel-default" id="pagination-activity">
+      <div class="panel-heading">
+        <h3 class="panel-title"><i class="glyphicon glyphicon-stats"></i> <?php echo $lang['Trends'] ?></h3>
+      </div>
+      <div class="panel-body"></div>
+      <table id="trends" ng-controller="trendCtrl" ng-table="tableParams" class="display" cellspacing="0" width="100%">
+
         <tbody>
-		<?php 
-            foreach($page['trends'] as $idx=>$entry) {
-        		$expires = ((is_null($entry['expires'])) ? " (".$lang['Never Expires'].") " : (" - ".$lang['Expires on']." " . date("D, F jS @ g:ia", strtotime($entry['expires']))));
-        		echo '<tr>';
-        			echo '<td>'.$entry['agefmt'].'</td>';
-        			if ( $mod_rewrite == true ) { 
-        				echo '<td><a href="'. $CONF['url'] . $entry['pid'] . '">' . $entry['title'] . '</a></td>'; } else { 
-        				echo '<td><a href="'. $CONF['url'] .'paste.php?paste='. $entry['pid'].'">' . $entry['title'] . '</a></td>'; }
-        			echo '<td>'.$expires.'</td>';
-        			echo '<td>'.$entry['format'].'</td>';
-        			echo '<td>'.round($entry['hits']/2).'</td>';
-        			echo '<td>'.$entry['agefmt'].'</td>';
-        		echo '</tr>';
-            }
-        ?>
+          <tr ng-repeat="paste in $data">
+            <td data-title="'#'"  sortable="'pid'">
+            {{paste.pid}} 
+            </td>
+            <td data-title="'Name'" sortable="'title'">
+              {{paste.title}} <div class="label label-info">{{paste.format}}</div>
+            </td>
+            <td data-title="'Hits'" sortable="'hits'">
+              {{paste.hits}}
+            </td>
+            <td data-title="'Date'" sortable="'age'">
+              {{paste.agefmt}}
+            </td>
+          </tr>
+          <?php 
+              //   foreach($page['trends'] as $idx=>$entry) {
+            		// $expires = ((is_null($entry['expires'])) ? " (".$lang['Never Expires'].") " : (" - ".$lang['Expires on']." " . date("D, F jS @ g:ia", strtotime($entry['expires']))));
+            		// echo '<tr>';
+            		// 	echo '<td>'.$entry['agefmt'].'</td>';
+            		// 	if ( $mod_rewrite == true ) { 
+            		// 		echo '<td><a href="'. $CONF['url'] . $entry['pid'] . '">' . $entry['title'] . '</a></td>'; } else { 
+            		// 		echo '<td><a href="'. $CONF['url'] .'paste.php?paste='. $entry['pid'].'">' . $entry['title'] . '</a></td>'; }
+            		// 	echo '<td>'.$expires.'</td>';
+            		// 	echo '<td>'.$entry['format'].'</td>';
+            		// 	echo '<td>'.round($entry['hits']/2).'</td>';
+            		// 	echo '<td>'.$entry['agefmt'].'</td>';
+            		// echo '</tr>';
+              //   }
+          ?>
 
         </tbody>
-    </table>
-<?php
+      </table>
+    </div>
+  </div>
+</div>
+  <?php
 } else {
-    echo "no";
+  echo "no";
 }
 ?>
 <!-- <script type="text/javascript">
